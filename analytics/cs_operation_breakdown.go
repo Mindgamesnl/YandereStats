6b0a7e3469b1060e94255a98e9310686ebddd122
Mindgamesnl <matsmoolhuizen@gmail.com>
2020-07-27 15:43:17 +0200

    Better display and counting

--- analytics/cs_operation_breakdown.go
+++ analytics/cs_operation_breakdown.go
@@ -71,7 +71,7 @@ func CSOperationBreakdown(cl changelog.ChangeLog)  {
 	for i := range list {
 		pair := list[i]
 
-		rows = append(rows, []string{"`" + pair.Key + "`", strconv.FormatInt(int64(pair.Value), 10), firstChanges[pair.Key]})
+		rows = append(rows, []string{"```c# " + pair.Key + "```", strconv.FormatInt(int64(pair.Value), 10), firstChanges[pair.Key]})
 	}
 
 	tableString := &strings.Builder{}
